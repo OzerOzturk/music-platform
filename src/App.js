8b73801a953f1c27d281a7330e8ddec2d5e4c7f2
OzerOzturk <ozerozturk1636@gmail.com>
2022-11-06 13:37:42 +0300

    feat(TopArtistsList.jsx): add TopArtistsList component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import TopArtistsList from './components/TopArtistsList/TopArtistsList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TopArtistsList />
     </div>
   );
 }
---
+++ src/components/TopArtistsList/TopArtistsList.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './TopArtistsList.css';
+import Artist from '../Artist/Artist';
+import axios from 'axios';
+import { v4 as uuid } from 'uuid';
+
+const TopArtistsList = () => {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${process.env.REACT_APP_API_KEY}&format=json`
+        );
+        console.log(response.data.artists);
+        setArtists(response.data.artists.artist);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <h1>Top Artists List</h1>
+      {artists && artists.map((artist) => <Artist artist={artist} key={uuid()} />)}
+    </main>
+  );
+};
+
+export default TopArtistsList;
