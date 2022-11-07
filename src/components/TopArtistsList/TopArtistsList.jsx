3cafbacb9d91f4b879a559ca7330675395215b02
OzerOzturk <ozerozturk1636@gmail.com>
2022-11-08 00:22:41 +0300

    feat: implement infinite scroll in top artist list.

--- src/components/TopArtistsList/TopArtistsList.jsx
+++ src/components/TopArtistsList/TopArtistsList.jsx
@@ -3,30 +3,44 @@ import './TopArtistsList.css';
 import Artist from '../Artist/Artist';
 import axios from 'axios';
 import { v4 as uuid } from 'uuid';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const TopArtistsList = () => {
   const [artists, setArtists] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${process.env.REACT_APP_API_KEY}&format=json&page=${page}`
+      );
+      //console.log(response.data.artists.artist);
+      setArtists([...artists, ...response.data.artists.artist]);
+      setPage(page + 1);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${process.env.REACT_APP_API_KEY}&format=json`
-        );
-        //console.log(response.data.artists.artist);
-        setArtists(response.data.artists.artist);
-      } catch (error) {
-        console.error(error.message);
-      }
-    };
-
     fetchData();
   }, []);
 
   return (
     <main className="topArtists container">
       <h1 className="topArtists__title">Top Artists List</h1>
-      {artists && artists.map((artist) => <Artist artist={artist} key={uuid()} />)}
+      <InfiniteScroll
+        dataLength={artists.length}
+        next={fetchData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }>
+        {artists && artists.map((artist) => <Artist artist={artist} key={uuid()} />)}
+      </InfiniteScroll>
     </main>
   );
 };
